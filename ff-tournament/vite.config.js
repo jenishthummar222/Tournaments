c5c4786f2188cda42f678e17b6a6728b047fe4b0
jenishthummar222 <jenishthummar222@gmail.com>
2026-05-19 17:27:10 +0530

    updated..

--- ff-tournament/vite.config.js
+++ ff-tournament/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite"
 
 
 // https://vite.dev/config/
@@ -10,5 +9,5 @@ export default defineConfig({
         sourcemap: false,
         minify: "esbuild"
     },
-    plugins: [react(), tailwindcss()],
+    plugins: [react()],
 })
